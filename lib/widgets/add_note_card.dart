d10cd2dbc6aefd1da3b8c8acdecd7a0468cdcab9
Quikler <romuchbinyk228@gmail.com>
2025-07-19 15:04:39 +0300

    feat: Add Ink widget to AddNoteCard to go to CreateNoteFormPage

--- lib/widgets/add_note_card.dart
+++ lib/widgets/add_note_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quik_note/pages/create_note_form_page.dart';
 import 'package:quik_note/svgs/common.dart';
 
 class AddNoteCard extends StatefulWidget {
@@ -12,36 +13,53 @@ class AddNoteCard extends StatefulWidget {
 }
 
 class _AddNoteCardState extends State<AddNoteCard> {
+  final BorderRadius _borderRadius = BorderRadius.all(Radius.circular(48));
+
+  void _handleTap() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => Material(child: CreateNoteFormPage())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      constraints: BoxConstraints(maxHeight: widget.maxHeight),
-      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Color(0x80FBFBF9),
-        borderRadius: BorderRadius.all(Radius.circular(48)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        spacing: 12,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            child: SvgPicture.string(plusCircleSvg, width: 48),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: _borderRadius,
+        onTap: _handleTap,
+        child: Ink(
+          padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+          decoration: BoxDecoration(
+            color: Color(0x80FBFBF9),
+            borderRadius: _borderRadius,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                textAlign: TextAlign.center,
-                "Add note",
-                style: TextStyle(color: Colors.grey, fontSize: 10),
-              ),
-            ],
+          child: Container(
+            constraints: BoxConstraints(maxHeight: widget.maxHeight),
+            height: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 12,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: SvgPicture.string(plusCircleSvg, width: 48),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.center,
+                      "Add note",
+                      style: TextStyle(color: Colors.grey, fontSize: 10),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
