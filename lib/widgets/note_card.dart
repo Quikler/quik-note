cc0dca665aea44b4aae51afbc7a14be98ebc154c
Quikler <romuchbinyk228@gmail.com>
2025-07-20 23:59:39 +0300

    feat: Implement _handleNoteDelete and forward it to parent in NoteCard

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:quik_note/data/db.dart';
 
 class NoteCard extends StatefulWidget {
   const NoteCard({
@@ -15,7 +16,7 @@ class NoteCard extends StatefulWidget {
   final String title;
   final String? content;
   final DateTime creationTime;
-  final Function(int? id) onNoteDelete;
+  final void Function(int? id) onNoteDelete;
 
   @override
   State<StatefulWidget> createState() {
@@ -29,6 +30,13 @@ class _NoteCardState extends State<NoteCard> {
     return formatter.format(widget.creationTime);
   }
 
+  void _handleNoteDelete() async {
+    final int count = await deleteNote(widget.id!);
+    if (count > 0) {
+      widget.onNoteDelete(widget.id);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,46 +44,59 @@ class _NoteCardState extends State<NoteCard> {
       padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       decoration: BoxDecoration(
         color: Color(0xFFFBFBF9),
-        borderRadius: BorderRadius.all(Radius.circular(48)),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 12,
+        //spacing: 12,
         children: [
           //Container(
-            //alignment: Alignment.center,
-            //child: Container(
-              //padding: EdgeInsets.all(12),
-              //decoration: BoxDecoration(
-                //color: Colors.cyan,
-                //boxShadow: [
-                  //BoxShadow(
-                    //color: Colors.cyan.withAlpha(100),
-                    //spreadRadius: 1,
-                    //blurRadius: 7,
-                    //offset: Offset(0, 4),
-                  //),
-                //],
-                //borderRadius: BorderRadius.all(Radius.circular(50)),
-              //),
-              //child: Icon(
-                //Icons.shopping_cart,
-                //color: Color(0xFFFBFBF9),
-                //size: 48,
-              //),
-            //),
+          //alignment: Alignment.center,
+          //child: Container(
+          //padding: EdgeInsets.all(12),
+          //decoration: BoxDecoration(
+          //color: Colors.cyan,
+          //boxShadow: [
+          //BoxShadow(
+          //color: Colors.cyan.withAlpha(100),
+          //spreadRadius: 1,
+          //blurRadius: 7,
+          //offset: Offset(0, 4),
+          //),
+          //],
+          //borderRadius: BorderRadius.all(Radius.circular(50)),
+          //),
+          //child: Icon(
+          //Icons.shopping_cart,
+          //color: Color(0xFFFBFBF9),
+          //size: 48,
+          //),
+          //),
           //),
           Column(
             spacing: 6,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                widget.title,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFF380099),
-                  fontWeight: FontWeight.w600,
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xFF380099),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.pink,
+                    onPressed: _handleNoteDelete,
+                    icon: Icon(Icons.delete),
+                  ),
+                ],
               ),
               Text(
                 _formatCreationTime(),
