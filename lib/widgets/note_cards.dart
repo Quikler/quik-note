dc35eaebd8a8567b9d46797fcf71fa7d9f1cccec
Quikler <romuchbinyk228@gmail.com>
2025-08-07 23:47:33 +0300

    feat: Add splash screen for app

---
+++ lib/widgets/note_cards.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quik_note/models/note.dart';
+
+class NoteCards extends StatelessWidget {
+  final List<Note> notes;
+
+  const NoteCards({super.key, required this.notes});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [],);
+  }
+  
+}
