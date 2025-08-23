5edde14728ef79448a45bafa9cf373a08cb7a42c
Quikler <romuchbinyk228@gmail.com>
2025-08-23 20:51:48 +0300

    feat: Add todo db model and db_todo functions to interact with it

---
+++ lib/data/db_todo.dart
@@ -0,0 +1,71 @@
+import 'package:quik_note/data/db.dart';
+import 'package:quik_note/models/todo.dart';
+import 'package:quik_note/utils/helpers.dart';
+
+Future<int> insertTodo(Todo todo) async {
+  final db = await getNotesDb();
+
+  return await db.insert('todos', todo.toMap());
+}
+
+Future<void> insertTodos(List<Todo> todos) async {
+  final db = await getNotesDb();
+  final batch = db.batch();
+
+  for (var todo in todos) {
+    batch.insert('todos', todo.toMap());
+  }
+
+  await batch.commit(noResult: true);
+  //final results = await batch.commit(noResult: true);
+  //return results.length;
+}
+
+Future<List<Todo>> getTodos([String? where, List<Object?>? whereArgs]) async {
+  final db = await getNotesDb();
+
+  final List<Map<String, Object?>> todoMaps = await db.query(
+    'todos',
+    where: where,
+    whereArgs: whereArgs,
+  );
+
+  return [
+    for (final {
+          'id': id as int,
+          'title': title as String,
+          'parentId': parentId as int?,
+          'checked': checked as int,
+          'completed': completed as int,
+        }
+        in todoMaps)
+      Todo(id, title, parentId, checked.toBool(), completed.toBool()),
+  ];
+}
+
+Future<int> updateTodo(Todo todo) async {
+  final db = await getNotesDb();
+
+  return await db.update(
+    'todos',
+    todo.toMap(),
+    where: 'id = ?',
+    whereArgs: [todo.id],
+  );
+}
+
+Future<int> deleteTodo(int id) async {
+  final db = await getNotesDb();
+
+  return await db.delete('todos', where: 'id = ?', whereArgs: [id]);
+}
+
+Future<int> deleteTodos(List<int> ids) async {
+  final db = await getNotesDb();
+
+  return await db.delete(
+    'todos',
+    where: 'id IN (${List.filled(ids.length, '?').join(',')})',
+    whereArgs: ids,
+  );
+}
---
+++ lib/models/todo.dart
@@ -0,0 +1,46 @@
+import 'package:quik_note/utils/helpers.dart';
+
+class Todo {
+  final int? id;
+  final String title;
+  final int? parentId;
+  final bool checked;
+  final bool completed;
+
+  const Todo(
+    this.id,
+    this.title,
+    this.parentId, [
+    this.checked = false,
+    this.completed = false,
+  ]);
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'parentId': parentId,
+      'checked': checked.toInt(),
+      'completed': completed.toInt(),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Todo{id: $id, title: $title, parentId: $parentId, checked: $checked, completed: $completed}';
+  }
+
+  Todo copyWith({
+    int? id,
+    String? title,
+    int? parentId,
+    bool? checked,
+    bool? completed,
+  }) => Todo(
+    id ?? this.id,
+    title ?? this.title,
+    parentId ?? this.parentId,
+    checked ?? this.checked,
+    completed ?? this.completed,
+  );
+}
