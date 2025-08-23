4392d07a415a572e2e168ac603538c569d887f1d
Quikler <romuchbinyk228@gmail.com>
2025-08-23 21:13:33 +0300

    feat: Add TodosList with retrieving todos from db

---
+++ lib/widgets/todos_list.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:quik_note/data/db_todo.dart';
+import 'package:quik_note/fill/custom_colors.dart';
+import 'package:quik_note/viewmodels/todo_vm.dart';
+import 'package:quik_note/widgets/add_todo_card.dart';
+import 'package:quik_note/widgets/todo_card.dart';
+import 'package:quik_note/wrappers/main_wrapper_margin.dart';
+
+class TodosList extends StatefulWidget {
+  const TodosList({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TodosListState();
+}
+
+class _TodosListState extends State<TodosList> {
+  List<TodoVm> _todos = [];
+
+  void _loadTodos() async {
+    final parentTodos = await getTodos('parentId IS NULL');
+    final parentVms = <TodoVm>[];
+
+    for (int i = 0; i < parentTodos.length; i++) {
+      final currentParent = parentTodos[i];
+      final parentChildren = await getTodos('parentId == ?', [
+        currentParent.id,
+      ]);
+
+      final parentChildrenVms = parentChildren.map(
+        (child) => TodoVm(child.id!, child.title, null),
+      );
+
+      final currentParentVm = TodoVm(
+        currentParent.id!,
+        currentParent.title,
+        parentChildrenVms,
+      );
+      parentVms.add(currentParentVm);
+    }
+
+    setState(() {
+      _todos = parentVms;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: MainWrapperMargin(
+        child: Column(
+          spacing: 16,
+          children: [
+            Column(
+              spacing: 16,
+              children: _todos.map((todo) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 12,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFBFBF9),
+                        border: BoxBorder.all(color: CustomColors.purple),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: TodoCard(todo: todo, children: todo.children),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+            AddTodoCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
