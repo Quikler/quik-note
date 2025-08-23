83fb26d082ae9e47a1aa528de8d3d0013aa4f260
Quikler <romuchbinyk228@gmail.com>
2025-08-23 20:51:12 +0300

    feat(db): Add todos table in notes.db

--- lib/data/db.dart
+++ lib/data/db.dart
@@ -5,9 +5,10 @@ Future<Database> getNotesDb() async {
   return openDatabase(
     join(await getDatabasesPath(), 'notes.db'),
     onCreate: (db, version) {
-      return db.execute(
-        'CREATE TABLE notes(id INTEGER PRIMARY KEY, title TEXT, content TEXT, starred BOOLEAN NOT NULL DEFAULT 0, creationTime DATETIME DEFAULT current_timestamp, lastEditedTime DATETIME)',
-      );
+      return db.execute('''
+            CREATE TABLE notes(id INTEGER PRIMARY KEY, title TEXT, content TEXT, starred BOOLEAN NOT NULL DEFAULT 0, creationTime DATETIME DEFAULT current_timestamp, lastEditedTime DATETIME);
+            CREATE TABLE todos(id INTEGER PRIMARY KEY, title TEXT, parentId INTEGER, checked BOOLEAN NOT NULL DEFAULT 0, completed BOOLEAN NOT NULL DEFAULT 0);
+        ''');
     },
     version: 1,
   );
